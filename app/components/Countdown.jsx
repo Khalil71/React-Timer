0e4b78b1f0ca1aa1529c9972a9f463722bc9b084
Khalil <mohamed.khalil609@gmail.com>
2017-12-31 19:58:04 +0200

    'add_navigation'

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+  render: function () {
+    return (
+      <h2>Countdown component</h2>
+    )
+  }
+});
+
+module.exports = Countdown;
